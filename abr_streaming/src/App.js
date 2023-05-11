7e5699db0c149bd3159fd16d58e41e33d613657d
ahmad.1 <ahmad.1@campus.tu-berlin.de>
2023-05-11 12:17:40 +0200

    first chart draft

--- abr_streaming/src/App.js
+++ abr_streaming/src/App.js
@@ -1,14 +1,13 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
-import Chart from './components/Chart';
+import Chart from 'chart.js/auto'
 import InputField from './components/InputField'; 
 import CustomButton from './components/Button'; 
 import {Container, InnerContainer} from './style'
 
-
 function App() {
   const [tragectories, setTragectories] = useState('ksl');
-  var ctx = document.getElementById('myChart'); //.getContext('2d');
+
 
 // Define the data for the chart
 var data = {
@@ -21,31 +20,45 @@ var data = {
     borderWidth: 1 // Border width of the bars
   }]
 };
-
-// Create the chart
-var CustomChart = new Chart(ctx, {
-  type: 'bar', // Type of chart (e.g., bar, line, pie)
-  data: data, // Data object we defined above
-  options: {
-    responsive: true, // Make the chart responsive
-    scales: {
-      y: {
-        beginAtZero: true // Start the y-axis at zero
+    var chartDiv = null;
+    var ctx ;
+    useEffect(()=>{
+    let myChart;
+    if(document ){
+    chartDiv = document.getElementById('myChart'); 
+    ctx = chartDiv.getContext('2d');
+    myChart = new Chart(ctx, {
+      type: 'bar', // Type of chart (e.g., bar, line, pie)
+      data: data, // Data object we defined above
+      options: {
+        responsive: true, // Make the chart responsive
+        scales: {
+          y: {
+            beginAtZero: true // Start the y-axis at zero
+          }
+        }
       }
-    }
-  }
-});
+    })
+
+}
+return ()=>{
+  myChart.destroy();
+}
+
+}, [])
+
+
+
 
   return (
     <div className="App">
       <Container container>
-      <InnerContainer xs={6}>
+      <InnerContainer item xs={5}>
         <InputField tragectories={tragectories} setTragectories={setTragectories} /> 
         <CustomButton/>  
       </InnerContainer>
-      <InnerContainer xs={6}>
-      <div style="width: 800px;"><canvas id="myChart"></canvas></div>
-        <CustomChart/>  
+      <InnerContainer item xs={5}>
+      <InnerContainer item><canvas id="myChart"/></InnerContainer> 
       </InnerContainer>
       </Container>
     </div>
--- abr_streaming/src/style.js
+++ abr_streaming/src/style.js
@@ -4,6 +4,9 @@ import { Box } from "@mui/material";
 
 export const Container = styled(Grid)`
 flex-direction:row; 
+align-items: flex-end;
+justify-content: space-around;
 `;
 export const InnerContainer = styled(Grid)`
+margin: 20px;
 `;
